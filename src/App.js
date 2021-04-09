eb990f6356a5d427b782e883ed4a451694736ab4
derrick matheny <mathenyderrick@gmail.com>
2021-04-08 22:37:40 -0400

    configuring the modal with its imports and styles from materialUI, need to still create the ability for user input

--- src/App.js
+++ src/App.js
@@ -4,10 +4,39 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Post from './Post';
 import { db } from './Firebase';
+import Modal from '@material-ui/core/Modal';
+import {makeStyles} from '@material-ui/core/styles'
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    height: "300px",
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    height: 200,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 /*  */
 function App() {
+  const classes=useStyles();
+  const [modalStyle]= useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen]= useState(false);
   // useEffect --> runs a piece of code based on a specific condition
 
   useEffect(() => {
@@ -19,9 +48,19 @@ function App() {
   
   //everytime the post changes or a new one is added it will run the code again
 
-//_____^^^^^^____________Current Problem is with the above code ________________
   return (
     <div className="app">
+
+      <Modal
+        open={open}
+        onClose={()=> setOpen(false)}// this listens to for clicks outside of the modal and closes if that's the case
+      >
+    <div style={modalStyle} className={classes.paper}>
+      <h2>I am a modal</h2>
+    </div>
+
+      </Modal>
+
     
         <div className="app__header">
             <img
@@ -33,7 +72,7 @@ function App() {
       <h1>This will most likely get me sued</h1>
             
             {/* you can think of this as looping through all the posts that are generated */}
-      {
+      { // literally when you use js stop forgetting to include the curly boys {}
         posts.map(({id ,post}) => (
           <Post key={id} username= {post.username} caption= {post.caption} imageURL={post.imageURL} />
         ))
