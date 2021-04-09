6958f778234db148de310c32e1d8898b48327f20
derrick matheny <mathenyderrick@gmail.com>
2021-04-08 23:36:45 -0400

    im having an issue getting the modal to align when i target the form using appsignup, will need to fix this tomorrow, but lots of progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Post from './Post';
 import { db } from './Firebase';
 import Modal from '@material-ui/core/Modal';
 import {makeStyles} from '@material-ui/core/styles'
+import {Button, Input,FormControl} from '@material-ui/core';
 
 function getModalStyle() {
   const top = 50;
@@ -38,6 +39,13 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [open, setOpen]= useState(false);
   // useEffect --> runs a piece of code based on a specific condition
+  
+  
+  //--- Setting up states for input fields----
+  const [username, setUsername]= useState("");
+  const [password, setPassword]= useState("");
+  const [email, setEmail]= useState("");
+  //--- Setting up states for input fields----
 
   useEffect(() => {
     db.collection("posts")
@@ -47,17 +55,60 @@ function App() {
   }, [])
   
   //everytime the post changes or a new one is added it will run the code again
+const signUp = (event)=>{
 
+}
   return (
     <div className="app">
 
       <Modal
+      //the user login code will reside within the modal
         open={open}
         onClose={()=> setOpen(false)}// this listens to for clicks outside of the modal and closes if that's the case
       >
     <div style={modalStyle} className={classes.paper}>
-      <h2>I am a modal</h2>
-    </div>
+
+    <form className="app__signup">
+      <center>
+        {/* adding instagram image to modal */}
+        <img 
+        className="app__headerImage"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt="instagram logo"
+        />
+
+
+        {/* Building the input fields */}
+        <Input
+        placeholder ="username"
+        type ="text"
+        value={username}
+        onChange={(e)=> setUsername(e.target.value)}
+        />
+
+        
+        <Input
+        placeholder ="email"
+        type ="text"
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
+        />
+
+        <Input
+        placeholder ="passsword"
+        type ="text"
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}
+        />
+
+      <Button onClick={signUp}>Sign Up!</Button>
+
+      </center>
+    </form>
+
+  </div>
+
+
 
       </Modal>
 
@@ -69,6 +120,9 @@ function App() {
               alt="instagram logo"
             />
           </div>
+            {/* Building User Input */}
+          <Button onClick={()=> setOpen(true)}>Sign Up!</Button>
+
       <h1>This will most likely get me sued</h1>
             
             {/* you can think of this as looping through all the posts that are generated */}
